bde7a9b57ad95324c99067f5d86fd3c74a7bd9c7
Mateus Voltolim <mateus.voltolim@gmail.com>
2020-01-10 19:57:50 -0300

    JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos

--- javascript-avancado-i/aluraframe/client/js/app/services/ConnectionFactory.js
+++ javascript-avancado-i/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,39 +1,57 @@
-var stores = ['negociacoes'];
-var version = 3;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function () {
+    const stores = ['negociacoes'];
+    const version = 3;
+    const dbName = 'aluraframe';
+    var connection = null;
+    var close = null;
 
-class ConnectionFactory {
+    class ConnectionFactory {
 
-    constructor() {
-        throw new Error("Não é possível criar instâncias de ConnectionFactory");
-    }
+        constructor() {
+            throw new Error("Não é possível criar instâncias de ConnectionFactory");
+        }
 
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
-            openRequest.onupgradeneeded = event => {
-                ConnectionFactory._createStores(event.target.result);
-            };
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+                openRequest.onupgradeneeded = event => {
+                    ConnectionFactory._createStores(event.target.result);
+                };
 
-            openRequest.onsuccess = event => {
-                resolve(event.target.result);
-            };
+                openRequest.onsuccess = event => {
+                    if (!connection) {
+                        connection = event.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function () {
+                            throw new Error("Você não pode fechar diretamente a conexão");
+                        }
+                    }
+                    resolve(event.target.result);
+                };
 
-            openRequest.onerror = event => {
-                console.log(event.target.error)
-                reject(event.target.error.name);
-            };
-        });
-    }
+                openRequest.onerror = event => {
+                    console.log(event.target.error)
+                    reject(event.target.error.name);
+                };
+            });
+        }
 
-    static _createStores(connection) {
-        stores.forEach(store => {
-            if (connection.objectStoreNames.contains(store)) {
-                connection.deleteObjectStore(store);
+        static closeConnections() {
+            if (connection) {
+                close();
+                connection = null;
             }
+        }
 
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
     }
-
-}
+})();
